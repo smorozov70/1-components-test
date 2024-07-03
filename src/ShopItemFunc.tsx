a2c9326e75deded9adc6a32118d3eafa6a9d6912
smorozov70 <SergMorozov@mail.ru>
2024-07-03 17:18:43 +0300

    first commit

---
+++ src/ShopItemFunc.tsx
@@ -0,0 +1,31 @@
+import '../css/main.css';
+
+interface Item {
+    brand: string;
+    title: string;
+    description: string;
+    descriptionFull: string;
+    price: number;
+    currency: string;
+  }
+  
+  interface ShopItemProps {
+    item: Item;
+  }
+  
+  export const ShopItemFunc: React.FC<ShopItemProps> = ({ item }) => {
+    return (
+      <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">{item.descriptionFull}</div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{item.currency}{item.price}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
